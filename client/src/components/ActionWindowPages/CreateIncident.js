670825c77c3afdc8c442509ed5b17595b30c54b3
James <james.rudd45@gmail.com>
2019-11-14 19:28:49 +1100

    pushed form into a grid

--- client/src/components/ActionWindowPages/CreateIncident.js
+++ client/src/components/ActionWindowPages/CreateIncident.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Moment from 'react-moment';
+import './CreateIncident.css';
 
 class CreateIncident extends Component {
   state = {
@@ -25,13 +26,31 @@ class CreateIncident extends Component {
 
   render() {
     return(
-      <form>
-        <span>Caller: </span><input type="text" name="client" value={this.state.client} onChange={this.handleInputChange} /><br />
-        <span>Assigned Team: </span><input type="text" name="techGroup" value={this.state.techGroup} onChange={this.handleInputChange} /><br />
-        <span>Assigned Tech: </span><input type="text" name="technician" value={this.state.technician} onChange={this.handleInputChange} /><br />
-        <span>State: </span> <input type="text" name="state" value={this.state.state} onChange={this.handleInputChange} /><br />
-        <span>Description: </span> <input type="text" name="description" value={this.state.description} onChange={this.handleInputChange} /><br />
-        <span>Short Description</span> <input type="text" name="shortDescription" value={this.state.shortDescription} onChange={this.handleInputChange} /><br />
+      <form id="incidentForm">
+        <div className="formField">
+          <span>Caller: </span>
+          <input type="text" name="client" value={this.state.client} onChange={this.handleInputChange} /><br />
+        </div>
+        <div className="formField">
+          <span>Assigned Team: </span>
+          <input type="text" name="techGroup" value={this.state.techGroup} onChange={this.handleInputChange} /><br />
+        </div>
+        <div className="formField">
+          <span>Assigned Tech: </span>
+          <input type="text" name="technician" value={this.state.technician} onChange={this.handleInputChange} /><br />
+        </div>
+        <div className="formField">
+          <span>State: </span>
+          <input type="text" name="state" value={this.state.state} onChange={this.handleInputChange} /><br />
+        </div>
+        <div className="formField">
+          <span>Description: </span>
+          <input type="text" name="description" value={this.state.description} onChange={this.handleInputChange} /><br />
+        </div>
+        <div className="formField">
+          <span>Short Description</span>
+          <input type="text" name="shortDescription" value={this.state.shortDescription} onChange={this.handleInputChange} /><br />
+        </div>
         {/* <span></span> <input type="" name="" value={} onChange={this.handleInputChange} /><br /> */}
         <button type="submit" onClick={this.incidentSubmit}>Submit Incident</button>
       </form>
