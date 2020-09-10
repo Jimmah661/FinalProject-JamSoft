da0ea78759bc446a84624fb95d7a31e98bd009d3
James Rudd <james.rudd45@gmail.com>
2020-09-10 08:08:39 +0800

    Updated to functional component using hooks

--- client/src/components/ActionWindowPages/manage.js
+++ client/src/components/ActionWindowPages/manage.js
@@ -1,29 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import API from '../../utilities/API'
 import {DragDropContext} from 'react-beautiful-dnd';
 import Column from './utils/Column';
 import UserColumn from './utils/UserColumn';
-import styled from 'styled-components';
 
-const Container = styled.div`
-display: flex;
-`;
-
-
-class Manage extends React.Component {
-    state = {
-        incidents: []
-    }
-    componentDidMount = () => {
-        API.reportAll()
-        .then(res => {
-          this.setState({incidents: res.data});
-        })
-        .catch(err => console.log(err));
+// class Manage extends React.Component {
+//     state = {
+//         incidents: []
+//     }
+//     componentDidMount = () => {
+//         API.reportAll()
+//         .then(res => {
+//           this.setState({incidents: res.data});
+//         })
+//         .catch(err => console.log(err));
         
-    }    
+//     }    
     
-    onDragEnd = result => {
+//     onDragEnd = result => {
+//         if (result.destination) {
+//             let updateDeets = {
+//                 _id: result.draggableId,
+//                 technician: result.destination.droppableId
+//                 }
+//             API.update(updateDeets)
+//                 .then(() => {
+//                     API.reportAll()
+//                         .then(res => {
+//                         this.setState({incidents: res.data});
+//                         })
+//                         .catch(err => console.log(err));
+//                 })
+//         } 
+//     }
+    
+//     render() {
+//         return (
+//             <div style={{display: 'flex'}}>
+//                 <DragDropContext onDragEnd={this.onDragEnd}>
+//                     <Column tasks={this.state.incidents} />
+//                     <UserColumn users={['James Rudd', 'Andrew Curve', "Sophia Gaunt"]} />
+//                 </DragDropContext>
+//             </div>
+//         )
+//     }
+// }
+
+const Manage = () => {
+    const [incidents, setIncidents] = useState([]);
+
+    useEffect(() => {
+        API.reportAll()
+            .then(res => {
+                setIncidents(res.data);
+            })
+            .catch(err => console.error(err))
+    }, []) 
+
+    const onDragEnd = result => {
         if (result.destination) {
             let updateDeets = {
                 _id: result.draggableId,
@@ -33,23 +67,21 @@ class Manage extends React.Component {
                 .then(() => {
                     API.reportAll()
                         .then(res => {
-                        this.setState({incidents: res.data});
+                            setIncidents(res.data);
                         })
                         .catch(err => console.log(err));
                 })
         } 
     }
-    
-    render() {
-        return (
-            <Container>
-                <DragDropContext onDragEnd={this.onDragEnd}>
-                    <Column tasks={this.state.incidents} />
-                    <UserColumn users={['James Rudd', 'Andrew Curve', "Sophia Gaunt"]} />
-                </DragDropContext>
-            </Container>
-        )
-    }
+
+    return(
+        <div style={{display: 'flex'}}>
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Column tasks={incidents} />
+                <UserColumn users={['James Rudd', 'Andrew Curve', "Sophia Gaunt"]} />
+            </DragDropContext>
+        </div>
+    )
 }
 
 export default Manage;
